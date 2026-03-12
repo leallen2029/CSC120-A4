17d39624f1d9ab73f610b04729e16bab34e11cc0
leallen2029 <leallen@smith.edu>
2026-03-12 17:40:03 -0400

    my

--- Engine.java
+++ Engine.java
@@ -1,3 +1,51 @@
-public class Engine {
+public class Engine implements EngineRequirements {
 
+    private FuelType fuelType;
+    private double currentFuelLevel;
+    private double maxFuelLevel;
+
+    // Constructor
+    public Engine(FuelType fuelType, double currentFuelLevel, double maxFuelLevel) {
+        this.fuelType = fuelType;
+        this.currentFuelLevel = currentFuelLevel;
+        this.maxFuelLevel = maxFuelLevel;
+    }
+
+    // Accessors
+    public FuelType getFuelType() {
+        return fuelType;
+    }
+
+    public double getCurrentFuelLevel() {
+        return currentFuelLevel;
+    }
+
+    public double getMaxFuelLevel() {
+        return maxFuelLevel;
+    }
+
+    // Refuel method
+    public void refuel() {
+        currentFuelLevel = maxFuelLevel;
+    }
+
+    // Go method
+    public boolean go() {
+        if (currentFuelLevel > 0) {
+            currentFuelLevel--;
+            System.out.println("Engine running. Remaining fuel: " + currentFuelLevel);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Test main
+    public static void main(String[] args) {
+        Engine myEngine = new Engine(FuelType.ELECTRIC, 0.0, 100.0);
+        while (myEngine.go()) {
+            System.out.println("Choo choo!");
+        }
+        System.out.println("Out of fuel.");
+    }
 }
